99e0844dff9fc42c4433d1465dbb14cc2d199d97
林牧賢 <michael.lin@welly.tw>
2024-01-31 13:29:22 +0800

    docs: 更新package.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,5 +41,13 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
     `gatsby-plugin-robots-txt`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
+        accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    `gatsby-plugin-image`,
   ],
 }
